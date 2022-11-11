0414db77ef27fb73284c2c6b0cb0a9cfd2ac6530
SonyaRafinatova <114873868+SonyaRafinatova@users.noreply.github.com>
2022-11-11 17:31:31 +0300

    Add files via upload

---
+++ Snake.py
@@ -0,0 +1,20 @@
+import pygame as pg
+import sys
+import random
+
+class Game():
+    """Класс для управления змейкой"""
+    def __init__(self):
+        self.screen_width = 1080
+        self.screen_height = 1080
+        bg_color = (0, 128, 0)
+        self.score = 0
+        
+
+    def update_screen(self):
+        self.screen.fill
+        self.update_screen()
+
+        if __name__ == "__main__":
+            game = Game()
+            game.run_game()
